27b3ad47c2862529cfb0e8c6b6d33fa4d5738332
Michael Bond <bond@datawaslost.net>
2021-02-01 11:42:30 -0500

    Update map.js

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -13,7 +13,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYXNseW9uczAwMSIsImEiOiJja2toZGhxN24wYTFrMm5xa
             }
         };
 
-        fetch('../json/mcd_hic_fc_p1.json')
+        fetch('public/json/mcd_hic_fc_p1.json')
             .then((res) => res.json())
             .then(data => formatToGeo(data))
 
